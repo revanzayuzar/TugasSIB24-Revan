448c88fb34dc4a30c6b67324f915d6dc6bebb93f
revanzayuzar <revanzayuzar03@gmail.com>
2024-03-26 09:50:05 +0700

    Memperbarui beberapa tampilan dan menambahkan background hitam saat navbar sedang muncul

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,11 @@ setTimeout(function() {
 // Open Menu / Menampilkan Menu
 let openMenu = document.getElementById("menu");
 let navbar = document.querySelector(".navbar");
+let bgDark = document.querySelector(".bg-dark");
 
 openMenu.addEventListener("click", function() {
     navbar.classList.add("navbar-active");
+    bgDark.classList.add("bg-dark-active");
 });
 
 // Close Menu / Menyembunyikan Menu
@@ -18,11 +20,13 @@ let closeMenu = document.getElementById("close");
 
 closeMenu.addEventListener("click", function() {
     navbar.classList.remove("navbar-active");
+    bgDark.classList.remove("bg-dark-active");
 });
 
 document.addEventListener("click", function(event) {
     if (!navbar.contains(event.target) && !closeMenu.contains(event.target) && !openMenu.contains(event.target)) {
         navbar.classList.remove("navbar-active");
+        bgDark.classList.remove("bg-dark-active");
     }
 });
 
